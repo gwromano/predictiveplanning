c9d8d6bf00195a170acb8c5098677b817a5ec7e2
George <gwromano@uark.edu>
2022-03-24 17:10:33 -0500

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -11,8 +11,8 @@ def index(request):
     # r = requests.get('https://httpbin.org/status/418')
     # print(r.text)
     # return HttpResponse('<pre>' + r.text + '</pre>')
-    #return render(request, "index.html")
-    return render("about-us.html", method=method)
+    return render(request, "about-us.html")
+
 
 
 def db(request):
