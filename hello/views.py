b026a9cd11f50dd177ddb19cb29d396aab1efd79
George <gwromano@uark.edu>
2022-03-24 17:02:11 -0500

    added requests

--- hello/views.py
+++ hello/views.py
@@ -1,16 +1,21 @@
+from django import apps
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, render_template
 from django.http import HttpResponse
+
 
 from .models import Greeting
 
 # Create your views here.
+@apps.route("/index.html", methods=["GET","POST"])
 def index(request):
+    method = request.method
     #return HttpResponse('Hello from Python!')
     # r = requests.get('https://httpbin.org/status/418')
     # print(r.text)
     # return HttpResponse('<pre>' + r.text + '</pre>')
-    return render(request, "index.html")
+    #return render(request, "index.html")
+    return render_template("index.html", method=method)
 
 
 def db(request):
