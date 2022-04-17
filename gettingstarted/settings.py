3af5d73836c174c20e5d97b980e3f55cf2739244
George <gwromano@uark.edu>
2022-04-16 21:49:57 -0500

    Revert "important amazon s3"
    This reverts commit 54aa77dc262a97be9d7fd67d481f06af0259d951.

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -13,17 +13,16 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import django_heroku
 
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "tWR6D;Lnjporc%$~M!hOkjKfZ!<m?t&_<fyOT$T)^mL-^pR;%[mSC|UhFxN"
+SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- gettingstarted/settings/dev.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "tWR6D;Lnjporc%$~M!hOkjKfZ!<m?t&_<fyOT$T)^mL-^pR;%[mSC|UhFxN"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
