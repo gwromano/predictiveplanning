b4376762f16e57fc4ba1e033b5f26d62f1fcce62
George <gwromano@uark.edu>
2022-04-16 21:48:21 -0500

    Update base.py

--- gettingstarted/settings/base.py
+++ gettingstarted/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = "tWR6D;Lnjporc%$~M!hOkjKfZ!<m?t&_<fyOT$T)^mL-^pR;%[mSC|UhFxN"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
