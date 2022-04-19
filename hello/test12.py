636e0e2e66663cd9ed735f78ae620b568e9e2333
George <gwromano@uark.edu>
2022-04-19 16:06:37 -0500

    Update test12.py

--- hello/test12.py
+++ hello/test12.py
@@ -1,9 +1,9 @@
 #for testing
 import tkinter
 
-top = tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+# top = tkinter.Tk()
+# # Code to add widgets will go here...
+# top.mainloop()
 
 def runArray(): 
     for i in range(0, 10):
