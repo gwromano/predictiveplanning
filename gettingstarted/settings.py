df3768bef43da47263cb0c5b2d96eaf873ef13f2
George <gwromano@uark.edu>
2022-04-19 09:41:53 -0500

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
-from decouple import gettingstarted
+#from decouple import gettingstarted
+
+import gettingstarted
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
