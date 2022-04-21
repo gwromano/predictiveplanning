cb5748b112584e74aae2005ec189bcce5122df4a
maggieturner <78766775+maggieturner@users.noreply.github.com>
2022-04-21 14:00:29 -0500

    update terminal addition

--- hello/views.py
+++ hello/views.py
@@ -3,12 +3,14 @@ import requests
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 import subprocess
-
+import webbrowser
+  
 from hello.test12 import runArray
 
 from .models import Greeting
 
-# Create your views here.
+#url = "https://raw.githubusercontent.com/gwromano/predictiveplanning/main/run.sh?token=GHSAT0AAAAAABTGWQ3KHPYS6IEIBLFQ67REYTBOZNA"
+
 def index(request):
     #return HttpResponse('Hello from Python!')
     # r = requests.get('https://httpbin.org/status/418')
@@ -24,8 +26,12 @@ def index(request):
 
 def makeOrder(request):
     if request.POST:    
-        #subprocess.call('/Users/maggieturner/Documents/Github/Capstone/run.sh') #maggie path
-        subprocess.call(r'C:\Users\georg\OneDrive\Documents\Visual Studio Projects\predictiveplanning\run.sh', shell=True) #george path
+        #url = "https://raw.githubusercontent.com/maggieturner/Capstone/main/run.sh?token=GHSAT0AAAAAABTGWQ3K73MIRHTSPRXQWCDWYTBRGSA"
+        #webbrowser.open_new(url)
+        #subprocess.call(url)
+        subprocess.call('/Users/maggieturner/Documents/Github/Capstone/run.sh') #maggie path
+        #subprocess.call(r'C:\Users\georg\OneDrive\Documents\Visual Studio Projects\predictiveplanning\run.sh', shell=True) #george path
+        #subprocess.Popen([r"/Users/maggieturner/Documents/Github/Capstone/run.sh", "https://raw.githubusercontent.com/maggieturner/Capstone/main/run.sh?token=GHSAT0AAAAAABTGWQ3LOSDTEW3D6EMPFEUCYTBQ2SA"])
     return render(request, "make-order.html")
     #return render(request, "about-us.html")
 
